8026ee037b776ba6c9bbbfc328b54eb0aa8e1654
muxcc <muxcc@aumm.ru>
2021-04-11 04:21:01 +0300

    new: add tag {kinopoisk_id} and {kinopoisk_url}

--- plexpy/common.py
+++ plexpy/common.py
@@ -531,6 +531,8 @@ NOTIFICATION_PARAMETERS = [
              {'name': 'Poster URL', 'type': 'str', 'value': 'poster_url', 'description': 'A URL for the movie, TV show, or album poster.'},
              {'name': 'Plex ID', 'type': 'str', 'value': 'plex_id', 'description': 'The Plex ID for the item.', 'example': 'e.g. 5d7769a9594b2b001e6a6b7e'},
              {'name': 'Plex URL', 'type': 'str', 'value': 'plex_url', 'description': 'The Plex URL to your server for the item.'},
+             {'name': 'Кинопоиск ID', 'type': 'str', 'value': 'kinopoisk_id', 'description': 'The Кинопоиск ID for the movie.', 'example': 'e.g. 1225587'},
+             {'name': 'Кинопоиск URL', 'type': 'str', 'value': 'kinopoisk_url', 'description': 'The Кинопоиск URL for the movie.'},
              {'name': 'IMDB ID', 'type': 'str', 'value': 'imdb_id', 'description': 'The IMDB ID for the movie.', 'example': 'e.g. tt2488496'},
              {'name': 'IMDB URL', 'type': 'str', 'value': 'imdb_url', 'description': 'The IMDB URL for the movie.'},
              {'name': 'TVDB ID', 'type': 'int', 'value': 'thetvdb_id', 'description': 'The TVDB ID for the TV show.', 'example': 'e.g. 121361'},
